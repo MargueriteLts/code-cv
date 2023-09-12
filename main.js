5ec6494a82cfaf7cbca2ff32d0f0128f19489113
MargueriteLts <marguerite.lorthiois@outlook.fr>
2023-09-12 21:10:25 +0300

    modif js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const covers = document.querySelectorAll(".cov");
 
+console.log(covers)
+
 covers.forEach(cover => {
   cover.addEventListener('click', () => {
+    console.log('cliked')
     document.querySelector('.hide').classList.remove('hide');
     cover.classList.add('hide');
   })
